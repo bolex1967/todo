46dfb4ea957e024c79b7290ae15acc1b805befbe
bolex1967 <bolex1967@gmail.com>
2020-07-01 19:01:16 +0300

    Filter

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
+"use strict";
 window.onload = initTodoList;
-
-const todoList = JSON.parse(localStorage.getItem("todoList")) || [];
-const ulElement = document.getElementById("out");
 const inputElement = document.getElementById("in");
+const radio = document.getElementsByName("radio");
+// const view = "2";
+const ulElement = document.getElementById("out");
+const todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
-function saveTodo() {
-  localStorage.setItem("todoList", JSON.stringify(todoList));
+function radioChange() {
+  console.log(this);
+  const view = this.value;
+  const ttt = document.querySelectorAll("ul li");
+  console.log("view: = ", view);
+  console.log(ttt);
+
+  for (let i = 0; i < ttt.length; i++) {
+    const liElement = ttt[i];
+    console.log(liElement);
+
+    if (view === "1") {
+      liElement.classList.remove("hide");
+    }
+
+    if (view === "2") {
+      if (liElement.classList.contains("checked")) {
+        liElement.classList.add("hide");
+      } else {
+        liElement.classList.remove("hide");
+      }
+    }
+
+    if (view === "3") {
+      if (!liElement.classList.contains("checked")) {
+        liElement.classList.add("hide");
+      } else {
+        liElement.classList.remove("hide");
+      }
+    }
+  }
 }
 
 function initTodoList() {
+  for (let i = 0; i < radio.length; i++) {
+    radio[i].onchange = radioChange;
+  }
+
   for (let todo of todoList) {
     // ulElement.append(renderTodo(todo));
     ulElement.prepend(renderTodo(todo));
   }
-
   inputElement.onkeypress = function (f) {
     if (f.keyCode === 13) {
       console.log("Enter");
       addItem();
     }
   };
+  radioChange.call({ value: "2" });
 }
+
+// {} - JS object
+// { abc: 1 }
+// this -> {}
+// this.value -> { value: "2" }
 
 function addItem() {
   const value = inputElement.value;
@@ -54,8 +94,10 @@ function checkTodo() {
 
   // if (todo.checked) {
   //   liElm.classList.add("checked");
+  //   liElm.classList.add("hide");
   // } else {
   //   liElm.classList.remove("checked");
+  //   liElm.classList.remove("hide");
   // }
   saveTodo();
 }
@@ -94,4 +136,8 @@ function renderTodo(todoItem) {
   liElm.append(inputElm);
   liElm.append(divElm);
   return liElm;
+}
+
+function saveTodo() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 }
